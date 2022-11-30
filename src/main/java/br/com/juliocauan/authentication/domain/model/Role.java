e74681139ceea6ad9bd83003ddfaa5f6693520af
juliocauan <juliocauan@gmail.com>
2022-11-30 09:50:19 -0300

    first commit

---
+++ src/main/java/br/com/juliocauan/authentication/domain/model/Role.java
@@ -0,0 +1,5 @@
+package br.com.juliocauan.authentication.domain.model;
+
+public interface Role {
+    String getName();
+}
---
+++ src/main/java/br/com/juliocauan/authentication/domain/model/User.java
@@ -0,0 +1,7 @@
+package br.com.juliocauan.authentication.domain.model;
+
+public interface User {
+    String getName();
+    String getEmail();
+    String getSecret();
+}

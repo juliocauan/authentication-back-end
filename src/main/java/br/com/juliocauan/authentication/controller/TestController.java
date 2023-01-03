b79985116ea78ed6e0d4383fd8ed9a284be858cd
juliocauan <juliocauan@gmail.com>
2023-01-03 11:39:45 -0300

    Implemented TestController

---
+++ src/main/java/br/com/juliocauan/authentication/controller/TestController.java
@@ -0,0 +1,22 @@
+package br.com.juliocauan.authentication.controller;
+
+import org.openapitools.api.TestApi;
+import org.openapitools.model.EnumRole;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public final class TestController implements TestApi {
+    
+    @Override
+    public ResponseEntity<String> _allAccess() {
+        return ResponseEntity.status(HttpStatus.OK).body("Public Content");
+    }
+
+    @Override
+    public ResponseEntity<String> _roleAccess(EnumRole role) {
+        return ResponseEntity.status(HttpStatus.OK).body(role.getValue() + "Board");
+    }
+    
+}

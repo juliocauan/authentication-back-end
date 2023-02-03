d42b73ff893cde02f2515b6489a2415e51fb884c
juliocauan <juliocauan@gmail.com>
2023-02-03 12:01:25 -0300

    Fixed role enum in init.sql

--- src/main/resources/docker/init.sql
+++ src/main/resources/docker/init.sql
@@ -1,7 +1,7 @@
 -- TODO add AUTH schema and create ENUM role
 CREATE SCHEMA auth;
 
-CREATE TYPE role AS ENUM ('ADMIN', 'MANAGER', 'USER');
+CREATE TYPE auth.role AS ENUM ('ADMIN', 'MANAGER', 'USER');
 
 CREATE TABLE auth.users
 (
@@ -13,7 +13,7 @@ CREATE TABLE auth.users
 CREATE TABLE auth.roles
 (
     id SMALLSERIAL PRIMARY KEY,
-    name role NOT NULL
+    name auth.role NOT NULL
 );
 CREATE TABLE auth.users_roles
 (

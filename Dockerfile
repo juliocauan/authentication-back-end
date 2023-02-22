667fd1f70186cb37f904d4993e041b7f73db0b82
juliocauan <juliocauan@gmail.com>
2023-02-22 19:45:33 +1000

    Implemented local test script

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ EXPOSE 8000
 WORKDIR /auth
 
 ENV DB_HOST=postgres DB_PORT=5432
-ENV DB_NAME=auth-db?useSSL=true DB_USER=root DB_PASSWORD=secret
+ENV DB_NAME=auth-db DB_USER=root DB_PASSWORD=secret
 
 COPY ./*.jar /authentication.jar
 
---
+++ test.sh
@@ -0,0 +1,7 @@
+export DB_HOST=localhost DB_PORT=5432 DB_NAME=test-auth-db DB_USER=root DB_PASSWORD=secret
+docker compose -f "src/main/resources/docker/docker-compose-test.yml" down
+docker volume rm --force VOLUME test-auth-db
+docker compose -f "src/main/resources/docker/docker-compose-test.yml" up -d --build
+./mvnw clean test -f pom.xml
+docker compose -f "src/main/resources/docker/docker-compose-test.yml" down
+docker volume rm --force VOLUME test-auth-db

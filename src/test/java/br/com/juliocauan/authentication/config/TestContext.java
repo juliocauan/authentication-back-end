76e98c1c956af99496d44750c76e07d2ec6a5d9d
juliocauan <juliocauan@gmail.com>
2023-02-22 19:50:52 +1000

    Removed unecessary instruction

--- src/test/java/br/com/juliocauan/authentication/config/TestContext.java
+++ src/test/java/br/com/juliocauan/authentication/config/TestContext.java
@@ -3,7 +3,6 @@ package br.com.juliocauan.authentication.config;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.openapitools.model.EnumRole;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestConstructor;
@@ -12,7 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import br.com.juliocauan.authentication.infrastructure.model.RoleEntity;
 import br.com.juliocauan.authentication.infrastructure.repository.RoleRepositoryImpl;
 import br.com.juliocauan.authentication.infrastructure.repository.UserRepositoryImpl;
 import lombok.AllArgsConstructor;
@@ -32,9 +30,6 @@ public class TestContext {
     @BeforeAll
     public void setup(){
         userRepository.deleteAll();
-        roleRepository.deleteAll();
-        for(EnumRole name : EnumRole.values())
-            roleRepository.save(RoleEntity.builder().name(name).build());
     }
 
     public MockMvc getMockMvc() {

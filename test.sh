a7cb2a4e8b9651d2e228774c348cdc29d68cd20e
juliocauan <juliocauan@gmail.com>
2023-03-05 00:57:37 +1000

    Small fix

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ export DB_HOST=localhost DB_PORT=5432 DB_NAME=test-auth-db DB_USER=root DB_PASSW
 docker compose -f "src/main/resources/docker/docker-compose-test.yml" down
 docker volume rm --force VOLUME test-auth-db
 docker compose -f "src/main/resources/docker/docker-compose-test.yml" up -d --build
-./mvnw -X test
+./mvnw test

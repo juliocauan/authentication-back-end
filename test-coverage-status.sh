d829be009714071eab19b80fc062a912f9b51bde
juliocauan <juliocauan@gmail.com>
2023-03-05 00:53:54 +1000

    test coverage and simple tests

--- test-coverage-status.sh
+++ test-coverage-status.sh
@@ -2,4 +2,10 @@ export DB_HOST=localhost DB_PORT=5432 DB_NAME=test-auth-db DB_USER=root DB_PASSW
 docker compose -f "src/main/resources/docker/docker-compose-test.yml" down
 docker volume rm --force VOLUME test-auth-db
 docker compose -f "src/main/resources/docker/docker-compose-test.yml" up -d --build
-./mvnw -X test
+./mvnw jacoco:prepare-agent clean test install jacoco:report
+if [ $? -eq 0 ]
+then
+    firefox target/site/jacoco/index.html &
+fi
+docker compose -f "src/main/resources/docker/docker-compose-test.yml" down
+docker volume rm --force VOLUME test-auth-db

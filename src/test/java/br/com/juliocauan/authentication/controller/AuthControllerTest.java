4b8ddc6093051465d444fc46741a5be2437a878a
juliocauan <juliocauan@gmail.com>
2023-03-08 15:57:19 +1000

    Creating tests for GetAllUsers method

--- src/test/java/br/com/juliocauan/authentication/controller/AuthControllerTest.java
+++ src/test/java/br/com/juliocauan/authentication/controller/AuthControllerTest.java
@@ -37,10 +37,11 @@ class AuthControllerTest extends TestContext {
     private final String urlSignin = "/api/auth/signin";
     private final String urlProfile = "/api/auth/profile";
     private final String urlAlterUserRole = "/api/auth/profile";
+    private final String urlGetAllUsers = "/api/auth/users";
     private final String headerAuthorization = "Authorization";
 
     private final String password = "1234567890";
-    private final String username = "test@email.com";
+    private final String username1 = "test@email.com";
     private final String username2 = "test2@email.com";
 
     private final String messageOk = "User registered successfully!";
@@ -61,8 +62,8 @@ class AuthControllerTest extends TestContext {
     @BeforeEach
     public void standard(){
         getUserRepository().deleteAll();
-        signupForm.password(password).username(username).role(EnumRole.USER);
-        signinForm.username(username).password(password);
+        signupForm.username(username1).password(password).role(EnumRole.USER);
+        signinForm.username(username1).password(password);
     }
 
     private final String getToken(String usernameToken, String passwordToken, EnumRole role){
@@ -100,7 +101,7 @@ class AuthControllerTest extends TestContext {
 
     @Test
     void givenDuplicatedUsername_WhenSignupUser_ThenEntityExistsException() throws Exception{
-        getUserRepository().save(UserEntity.builder().password(password).username(username).roles(null).build());
+        getUserRepository().save(UserEntity.builder().password(password).username(username1).roles(null).build());
         getMockMvc().perform(
             post(urlSignup)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -137,7 +138,7 @@ class AuthControllerTest extends TestContext {
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.token", hasLength(143)))
             .andExpect(jsonPath("$.type").value("Bearer"))
-            .andExpect(jsonPath("$.username").value(username))
+            .andExpect(jsonPath("$.username").value(username1))
             .andExpect(jsonPath("$.roles", hasSize(1)))
             .andExpect(jsonPath("$.roles[0]").value(EnumRole.USER.getValue()));
     }
@@ -154,7 +155,7 @@ class AuthControllerTest extends TestContext {
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.token", hasLength(143)))
             .andExpect(jsonPath("$.type").value("Bearer"))
-            .andExpect(jsonPath("$.username").value(username))
+            .andExpect(jsonPath("$.username").value(username1))
             .andExpect(jsonPath("$.roles", hasSize(1)))
             .andExpect(jsonPath("$.roles[0]").value(EnumRole.ADMIN.getValue()));
     }
@@ -173,35 +174,35 @@ class AuthControllerTest extends TestContext {
 
     @Test
     void givenLoggedUser_WhenProfileContent_ThenProfile() throws Exception{
-        String token = getToken(username, password, EnumRole.USER);
+        String token = getToken(username1, password, EnumRole.USER);
         getMockMvc().perform(
             get(urlProfile)
                 .header(headerAuthorization, token))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.username").value(username));
+            .andExpect(jsonPath("$.username").value(username1));
     }
 
     @Test
     void givenLoggedAdmin_WhenProfileContent_ThenProfile() throws Exception{
-        String token = getToken(username, password, EnumRole.ADMIN);
+        String token = getToken(username1, password, EnumRole.ADMIN);
         getMockMvc().perform(
             get(urlProfile)
                 .header(headerAuthorization, token))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.username").value(username));
+            .andExpect(jsonPath("$.username").value(username1));
     }
 
     @Test
     void givenLoggedManager_WhenProfileContent_ThenProfile() throws Exception{
-        String token = getToken(username, password, EnumRole.MANAGER);
+        String token = getToken(username1, password, EnumRole.MANAGER);
         getMockMvc().perform(
             get(urlProfile)
                 .header(headerAuthorization, token))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.username").value(username));
+            .andExpect(jsonPath("$.username").value(username1));
     }
 
     @Test
@@ -214,8 +215,13 @@ class AuthControllerTest extends TestContext {
     }
 
     @Test
-    void givenNotAuthenticatedAdmin_WhenAlterUserRole_ThenUnauthorized() throws Exception{
-        String token = getToken(username, password, EnumRole.USER);
+    void givenNotAuthenticatedAdmin_WhenAlterUserRole_ThenForbidden() throws Exception{
+        String token = getToken(username1, password, EnumRole.USER);
+        getMockMvc().perform(
+            patch(urlAlterUserRole)
+                .header(headerAuthorization, token))
+            .andExpect(status().isForbidden());
+        token = getToken(username2, password, EnumRole.MANAGER);
         getMockMvc().perform(
             patch(urlAlterUserRole)
                 .header(headerAuthorization, token))
@@ -224,7 +230,7 @@ class AuthControllerTest extends TestContext {
 
     @Test
     void givenNotPresentUsername_WhenAlterUserRole_Then404AndMessage() throws Exception{
-        String tokenAdmin = getToken(username, password, EnumRole.ADMIN);
+        String tokenAdmin = getToken(username1, password, EnumRole.ADMIN);
         Set<EnumRole> roles = new HashSet<>();
         roles.add(EnumRole.USER);
         ProfileRoles profileRoles = new ProfileRoles().username(username2).roles(roles);
@@ -242,11 +248,11 @@ class AuthControllerTest extends TestContext {
 
     @Test
     void givenProfileRoles_WhenAlterUserRole_Then200AndProfileRoles() throws Exception{
-        authController._signupUser(signupForm);
         String tokenAdmin = getToken(username2, password, EnumRole.ADMIN);
+        authController._signupUser(signupForm);
         Set<EnumRole> roles = new HashSet<>();
         for(EnumRole role : EnumRole.values()) roles.add(role);
-        ProfileRoles profileRoles = new ProfileRoles().username(username).roles(roles);
+        ProfileRoles profileRoles = new ProfileRoles().username(username1).roles(roles);
         getMockMvc().perform(
             patch(urlAlterUserRole)
                 .header(headerAuthorization, tokenAdmin)
@@ -254,8 +260,44 @@ class AuthControllerTest extends TestContext {
                 .content(getObjectMapper().writeValueAsString(profileRoles)))
             .andExpect(status().isOk())
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$.username").value(username))
+            .andExpect(jsonPath("$.username").value(username1))
             .andExpect(jsonPath("$.roles", hasSize(EnumRole.values().length)));
+    }
+
+    @Test
+    void givenNotAuthenticatedAdmin_WhenGetAllUsers_ThenForbidden() throws Exception{
+        String token = getToken(username1, password, EnumRole.USER);
+        getMockMvc().perform(
+            get(urlGetAllUsers)
+                .header(headerAuthorization, token))
+            .andExpect(status().isForbidden());
+        token = getToken(username2, password, EnumRole.MANAGER);
+        getMockMvc().perform(
+            get(urlGetAllUsers)
+                .header(headerAuthorization, token))
+            .andExpect(status().isForbidden());
+    }
+
+    //TODO review token variable
+
+    @Test
+    void givenNothing_WhenGetAllUsers_Then200AndList() throws Exception{
+        //TODO
+    }
+
+    @Test
+    void givenUsername_WhenGetAllUsers_Then200AndList() throws Exception{
+        //TODO
+    }
+
+    @Test
+    void givenRole_WhenGetAllUsers_Then200AndList() throws Exception{
+        //TODO
+    }
+
+    @Test
+    void givenUsernameAndRole_WhenGetAllUsers_Then200AndList() throws Exception{
+        //TODO
     }
 
 }

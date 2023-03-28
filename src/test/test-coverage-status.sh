43ed34f15dd99897a01d5c694a9a987d44f8519c
juliocauan <juliocauan@gmail.com>
2023-03-28 12:35:36 +1000

    Updated docker-compose.yml location

--- src/test/test-coverage-status.sh
+++ src/test/test-coverage-status.sh
@@ -1,11 +1,11 @@
 export DB_HOST=localhost DB_PORT=5432 DB_NAME=test-auth-db DB_USER=root DB_PASSWORD=secret
-docker compose -f "src/main/resources/docker/docker-compose-test.yml" down
+docker compose -f "docker-compose.yml" down
 docker volume rm --force VOLUME test-auth-db
-docker compose -f "src/main/resources/docker/docker-compose-test.yml" up -d --build
+docker compose -f "docker-compose.yml" up -d --build
 ./mvnw jacoco:prepare-agent clean package jacoco:report
 if [ $? -eq 0 ]
 then
     firefox target/site/jacoco/index.html &
 fi
-docker compose -f "src/main/resources/docker/docker-compose-test.yml" down
+docker compose -f "docker-compose.yml" down
 docker volume rm --force VOLUME test-auth-db

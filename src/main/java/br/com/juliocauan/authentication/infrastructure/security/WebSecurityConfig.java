5753107fbcd154434ea05c18d94d9138473d279d
juliocauan <juliocauan@gmail.com>
2023-04-26 15:46:00 +1000

    Updated cors

--- src/main/java/br/com/juliocauan/authentication/infrastructure/security/WebSecurityConfig.java
+++ src/main/java/br/com/juliocauan/authentication/infrastructure/security/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import org.openapitools.model.EnumRole;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -63,15 +62,15 @@ public class WebSecurityConfig {
     }
     
     @Bean
-    FilterRegistrationBean<CorsFilter> corsFilter() {
+    public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
         config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         source.registerCorsConfiguration("/**", config);
-        return new FilterRegistrationBean<>(new CorsFilter(source));
+        return new CorsFilter(source);
     }
     
     @Bean

d9a7bfa6ccd692fbff2358d27fb2f2fac412767c
juliocauan <juliocauan@gmail.com>
2023-10-17 18:45:52 -0300

    Implemented test environment

---
+++ src/test/resources/init.sql
@@ -0,0 +1,32 @@
+CREATE SCHEMA auth;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE auth.users
+(
+    id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(120) NOT NULL
+);
+CREATE TABLE auth.roles
+(
+    id SMALLSERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL
+);
+CREATE TABLE auth.users_roles
+(
+    user_id uuid REFERENCES auth.users(id),
+    role_id SMALLINT REFERENCES auth.roles(id),
+    PRIMARY KEY(user_id, role_id)
+);
+CREATE TABLE auth.recovery_tokens
+(
+    id SERIAL PRIMARY KEY,
+    user_id uuid REFERENCES auth.users(id) ON DELETE CASCADE UNIQUE NOT NULL,
+    token VARCHAR(43) NOT NULL,
+    expire_date timestamp NOT NULL
+);
+
+INSERT INTO auth.roles(name) VALUES
+    ('ADMIN'),
+    ('MANAGER'),
+    ('USER');
--- src/test/test-coverage-status.sh
+++ src/test/test-coverage-status.sh
@@ -1,20 +1,10 @@
-source .env
-
-# Exporta as variáveis de ambiente
-# TODO make test, dev and prod ambience
-export DB_HOST
-export DB_PORT
-export DB_NAME
-export DB_USER
-export DB_PASSWORD
-
-docker compose -f "docker-compose.yml" down
+docker compose -f "docker-compose-test.yml" down
 docker volume rm --force VOLUME test-auth-db
-docker compose -f "docker-compose.yml" up -d --build
+docker compose -f "docker-compose-test.yml" up -d --build
 ./mvnw jacoco:prepare-agent clean package jacoco:report
 if [ $? -eq 0 ]
 then
     firefox target/site/jacoco/index.html &
 fi
-docker compose -f "docker-compose.yml" down
+docker compose -f "docker-compose-test.yml" down
 docker volume rm --force VOLUME test-auth-db

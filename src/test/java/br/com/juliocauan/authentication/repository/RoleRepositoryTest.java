a08ea10fde8704ba46376e91417eb9308e4c91c1
juliocauan <juliocauan@gmail.com>
2023-10-25 10:21:34 -0300

    RoleRepository refactored and tested

--- src/test/java/br/com/juliocauan/authentication/repository/RoleRepositoryTest.java
+++ src/test/java/br/com/juliocauan/authentication/repository/RoleRepositoryTest.java
@@ -1,7 +1,6 @@
 package br.com.juliocauan.authentication.repository;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,22 +13,15 @@ import br.com.juliocauan.authentication.infrastructure.repository.UserRepository
 
 class RoleRepositoryTest extends TestContext {
 
-    private RoleEntity entity;
-
     public RoleRepositoryTest(UserRepositoryImpl userRepository, RoleRepositoryImpl roleRepository,
             ObjectMapper objectMapper, MockMvc mockMvc) {
         super(userRepository, roleRepository, objectMapper, mockMvc);
     }
 
-    @Override @BeforeAll
-    public void setup() {
-        super.setup();
-        entity = getRoleRepository().findAll().get(0);
-    }
-
     @Test
     void givenPresentName_WhenFindByName_ThenRole(){
-        Assertions.assertEquals(entity, getRoleRepository().findByName(entity.getName()).get());
+        RoleEntity expectedEntity = getRoleRepository().findAll().get(0);
+        Assertions.assertEquals(expectedEntity, getRoleRepository().findByName(expectedEntity.getName()).get());
     }
 
     @Test

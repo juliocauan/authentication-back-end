2f592555cedc781c534c5fd8da49e1685f114877
juliocauan <juliocauan@gmail.com>
2023-10-30 10:02:39 -0300

    ProfileController refactored and tested

--- src/test/java/br/com/juliocauan/authentication/controller/ProfileControllerTest.java
+++ src/test/java/br/com/juliocauan/authentication/controller/ProfileControllerTest.java
@@ -6,137 +6,113 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.HashSet;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openapitools.model.EnumRole;
 import org.openapitools.model.JWTResponse;
 import org.openapitools.model.PasswordMatch;
 import org.openapitools.model.PasswordUpdateForm;
-import org.openapitools.model.SigninForm;
-import org.openapitools.model.SignupForm;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.juliocauan.authentication.config.TestContext;
+import br.com.juliocauan.authentication.infrastructure.model.UserEntity;
 import br.com.juliocauan.authentication.infrastructure.repository.RoleRepositoryImpl;
 import br.com.juliocauan.authentication.infrastructure.repository.UserRepositoryImpl;
+import br.com.juliocauan.authentication.infrastructure.service.application.JwtServiceImpl;
 
 class ProfileControllerTest extends TestContext {
 
-    private final AuthController authController;
+    private final JwtServiceImpl jwtService;
+    private final PasswordEncoder encoder;
 
     private final String urlProfile = "/api/auth/profile";
-    private final String headerAuthorization = "Authorization";
+    private final String authorizationHeader = "Authorization";
 
+    private final String username = "test@email.com";
     private final String password = "1234567890";
     private final String newPassword = "1234567890123";
-    private final String username1 = "test1@email.com";
 
     private final String updatedPasswordMessage = "Password updated successfully!";
-    private final String oldPasswordError = "Wrong current password!";
-    private final String newPasswordError = "Confirmation and new password are different!";
-    private final String notAllowedError = "Not Allowed!";
-
-    private String token;
+    private final String errorPassword = "Wrong current password!";
+    private final String errorPasswordMatch = "Confirmation and new password are different!";
+    private final String errorNotAllowed = "Not Allowed!";
 
     public ProfileControllerTest(UserRepositoryImpl userRepository, RoleRepositoryImpl roleRepository,
-            ObjectMapper objectMapper, MockMvc mockMvc, AuthController authController) {
+            ObjectMapper objectMapper, MockMvc mockMvc, JwtServiceImpl jwtService, PasswordEncoder encoder) {
         super(userRepository, roleRepository, objectMapper, mockMvc);
-        this.authController = authController;
+        this.jwtService = jwtService;
+        this.encoder = encoder;
     }
 
     @BeforeEach
     public void standard(){
         getUserRepository().deleteAll();
-        token = null;
+        getUserRepository().save(UserEntity
+            .builder()
+                .id(null)
+                .username(username)
+                .password(encoder.encode(password))
+                .roles(new HashSet<>())
+            .build());
     }
 
-    private final String getToken(String usernameToken, EnumRole role){
-        SignupForm signup = new SignupForm().username(usernameToken).password(password).role(role);
-        SigninForm signin = new SigninForm().username(usernameToken).password(password);
-        authController._signupUser(signup);
-        JWTResponse response = authController._signinUser(signin).getBody();
-        return response == null ? null : "Bearer " + response.getToken();
-    }
-
-    @Test
-    void givenAnyLoggedUser_WhenProfileContent_ThenProfile() throws Exception{
-        for(EnumRole role : EnumRole.values()){
-            getUserRepository().deleteAll();
-            token = getToken(username1, role);
-            getMockMvc().perform(
-                get(urlProfile)
-                    .header(headerAuthorization, token))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.username").value(username1));
-        }
+    private final String getToken(){
+        JWTResponse jwt = jwtService.authenticate(username, password);
+        return jwt.getType() + " " + jwt.getToken();
     }
 
     @Test
-    void givenUnlogged_WhenProfileContent_ThenUnauthorized() throws Exception{
+    void profileContent() throws Exception{
+        getMockMvc().perform(
+            get(urlProfile)
+                .header(authorizationHeader, getToken()))
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.username").value(username));
+    }
+
+    @Test
+    void profileContent_error_notLogged() throws Exception{
         getMockMvc().perform(
             get(urlProfile))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isUnauthorized())
-            .andExpect(jsonPath("$.message").value(notAllowedError));
+            .andExpect(jsonPath("$.message").value(errorNotAllowed));
     }
 
     @Test
-    void givenAnyLoggedUser_WhenAlterUserPassword_ThenMessage() throws Exception{
+    void alterUserPassword() throws Exception{
         PasswordUpdateForm passwordUpdateForm = new PasswordUpdateForm()
             .oldPassword(password)
             .newPasswordMatch(new PasswordMatch()
                 .password(newPassword)
                 .passwordConfirmation(newPassword));
-        for(EnumRole role : EnumRole.values()){
-            getUserRepository().deleteAll();
-            token = getToken(username1, role);
-            getMockMvc().perform(
-                patch(urlProfile)
-                    .header(headerAuthorization, token)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(getObjectMapper().writeValueAsString(passwordUpdateForm)))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.body").value(updatedPasswordMessage));
-        }
+        getMockMvc().perform(
+            patch(urlProfile)
+                .header(authorizationHeader, getToken())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(getObjectMapper().writeValueAsString(passwordUpdateForm)))
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.body").value(updatedPasswordMessage));
     }
 
     @Test
-    void givenUnlogged_WhenAlterUserPassword_ThenUnauthorized() throws Exception{
+    void alterUserPassword_error_notLogged() throws Exception{
         getMockMvc().perform(
             patch(urlProfile))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isUnauthorized())
-            .andExpect(jsonPath("$.message").value(notAllowedError));
+            .andExpect(jsonPath("$.message").value(errorNotAllowed));
     }
 
     @Test
-    //TODO refactor test to passwordServiceTest
-    void givenWrongOldPassword_WhenAlterUserPassword_ThenErrorMessage() throws Exception{
-        token = getToken(username1, EnumRole.USER);
-        PasswordUpdateForm passwordUpdateForm = new PasswordUpdateForm()
-            .oldPassword(newPassword)
-            .newPasswordMatch(new PasswordMatch()
-                .password(newPassword)
-                .passwordConfirmation(newPassword));
-        getMockMvc().perform(
-            patch(urlProfile)
-                .header(headerAuthorization, token)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(getObjectMapper().writeValueAsString(passwordUpdateForm)))
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.message").value(oldPasswordError));
-    }
-
-    @Test
-    //TODO refactor test to passwordServiceTest
-    void givenWrongConfirmationPassword_WhenAlterUserPassword_ThenErrorMessage() throws Exception{
-        token = getToken(username1, EnumRole.USER);
+    void alterUserPassword_error_passwordMatch() throws Exception{
         PasswordUpdateForm passwordUpdateForm = new PasswordUpdateForm()
             .oldPassword(password)
             .newPasswordMatch(new PasswordMatch()
@@ -144,12 +120,29 @@ class ProfileControllerTest extends TestContext {
                 .passwordConfirmation(password));
         getMockMvc().perform(
             patch(urlProfile)
-                .header(headerAuthorization, token)
+                .header(authorizationHeader, getToken())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(getObjectMapper().writeValueAsString(passwordUpdateForm)))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.message").value(newPasswordError));
+            .andExpect(jsonPath("$.message").value(errorPasswordMatch));
+    }
+
+    @Test
+    void alterUserPassword_error_invalidPassword() throws Exception{
+        PasswordUpdateForm passwordUpdateForm = new PasswordUpdateForm()
+            .oldPassword(newPassword)
+            .newPasswordMatch(new PasswordMatch()
+                .password(newPassword)
+                .passwordConfirmation(newPassword));
+        getMockMvc().perform(
+            patch(urlProfile)
+                .header(authorizationHeader, getToken())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(getObjectMapper().writeValueAsString(passwordUpdateForm)))
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value(errorPassword));
     }
 
 }

458a4533d0d8cbf890cb1546752011641c4a8bd0
juliocauan <juliocauan@gmail.com>
2023-10-30 16:01:34 -0300

    UserDetailsService refactored and tested

--- src/main/java/br/com/juliocauan/authentication/infrastructure/security/service/UserDetailsServiceImpl.java
+++ src/main/java/br/com/juliocauan/authentication/infrastructure/security/service/UserDetailsServiceImpl.java
@@ -2,10 +2,8 @@ package br.com.juliocauan.authentication.infrastructure.security.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import br.com.juliocauan.authentication.domain.model.User;
 import br.com.juliocauan.authentication.infrastructure.model.mapper.UserMapper;
 import br.com.juliocauan.authentication.infrastructure.service.UserServiceImpl;
 import jakarta.transaction.Transactional;
@@ -19,9 +17,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     
     @Override
     @Transactional
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.getByUsername(username);
-        return UserMapper.domainToUserPrincipal(user);
+    public UserDetails loadUserByUsername(String username) {
+        return UserMapper.domainToUserPrincipal(userService.getByUsername(username));
     }
     
 }

da8834bcd1139159285c7db3028889adc4d22df4
juliocauan <juliocauan@gmail.com>
2023-11-06 15:42:45 -0300

    Refactored docker-compose and .env

--- src/test/build.sh
+++
@@ -1,5 +0,0 @@
-bash src/test/package.sh
-docker compose -f "docker-compose.yml" down
-docker image rm --force IMAGE authentication-back-end-dev-app:latest
-docker volume rm --force VOLUME auth-db-dev
-docker compose -f "docker-compose.yml" up -d --build

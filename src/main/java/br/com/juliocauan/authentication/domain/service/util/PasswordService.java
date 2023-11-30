086ae83177cc5571126f2462c628b9e0601acac5
juliocauan <juliocauan@gmail.com>
2023-11-30 15:43:23 -0300

    Created PasswordServiceImpl to deal with Infra

--- src/main/java/br/com/juliocauan/authentication/domain/service/util/PasswordService.java
+++ src/main/java/br/com/juliocauan/authentication/domain/service/util/PasswordService.java
@@ -1,33 +1,25 @@
 package br.com.juliocauan.authentication.domain.service.util;
 
 import org.openapitools.model.PasswordMatch;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
 import br.com.juliocauan.authentication.infrastructure.exception.InvalidPasswordException;
 import br.com.juliocauan.authentication.infrastructure.exception.PasswordMatchException;
-import lombok.AllArgsConstructor;
 
-@Service
-@AllArgsConstructor
-public final class PasswordService {
-
-    private final PasswordEncoder encoder;
+public abstract class PasswordService {
 
     public final void checkPasswordConfirmation(PasswordMatch passwordMatch){
         String newPassword = encode(passwordMatch.getPassword());
         String confirmationPassword = passwordMatch.getPasswordConfirmation();
-        if(!encoder.matches(confirmationPassword, newPassword))
+        if(!matches(confirmationPassword, newPassword))
             throw new PasswordMatchException("Confirmation and new password are different!");
     }
 
     public final void checkCurrentPassword(String encodedPassword, String rawPassword){
-        if(!encoder.matches(rawPassword, encodedPassword))
+        if(!matches(rawPassword, encodedPassword))
             throw new InvalidPasswordException("Wrong current password!");
     }
 
-    public final String encode(String password) {
-        return encoder.encode(password);
-    }
+    public abstract String encode(String password);
+    public abstract boolean matches(String rawPassword, String encodedPassword);
 
 }

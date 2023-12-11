dfe21ef236f2b225cc17eb58aad39ce98b6a7c7e
juliocauan <juliocauan@gmail.com>
2023-12-11 11:49:18 -0300

    refactor: production environment

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,6 +1,6 @@
 docker compose -f "docker-compose-dev.yml" down
 docker volume rm --force VOLUME auth-dev
-docker image rm authentication-dev-app:latest
+docker image rm authentication-dev-server:latest
 docker image prune --force
 
 ./mvnw clean package -DskipTests

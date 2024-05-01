d6141c101aa266ef1e6dab54f090f4da28542bd7
juliocauan <juliocauan@gmail.com>
2024-05-01 17:21:33 -0300

    Refactor - UserRepository: removed @Modifying and @Query

--- src/main/java/br/com/juliocauan/authentication/infrastructure/repository/UserRepository.java
+++ src/main/java/br/com/juliocauan/authentication/infrastructure/repository/UserRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,7 +34,7 @@ public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecifi
             .stream().collect(Collectors.toList());
     }
 
-    //TODO refactor
+    //TODO REFACTOR
     default List<User> findAllByRole(String roleName) {
         return this.findAll(Specification
                 .where(hasRole(roleName)))
@@ -58,7 +56,7 @@ public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecifi
         this.save(user);
     }
 
-    //TODO refactor
+    //TODO REFACTOR
     default void updatePassword(User user, String newPassword) {
         PasswordUtil.validateSecurity(newPassword);
         user.setPassword(PasswordUtil.encode(newPassword));
@@ -66,8 +64,6 @@ public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecifi
     }
 
     @Transactional
-    @Modifying
-    @Query("DELETE FROM User u WHERE u.username = :username")
     void deleteByUsername(String username);
 
 }
